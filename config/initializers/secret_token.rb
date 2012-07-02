1027bcd9f23d7e5f5accba89b0e12e5e6f072e03
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-07-02 19:03:25 +0300

    Modify Git Ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aimsell::Application.config.secret_token = 'f86bc63b03cdd2a2329e7356cb41de95fe9640e84103be29ed14b9b09063892afcf22501cb30916653eb8fd629887e02a84c13654e6e7746f4ce73cfc61c39d5'
