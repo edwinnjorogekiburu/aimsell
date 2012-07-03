c0a945353f541b2a5d3114a4075d55ddcdc42318
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-07-03 17:35:40 +0300

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Aimsell::Application.routes.draw do
+
+  root to: 'static_pages#home'
+ 
+  match '/home', to: 'static_pages#home'
+
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
