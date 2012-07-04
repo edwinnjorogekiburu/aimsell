442d6e218cd4ddf7926629203914523c6309354e
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-07-04 14:24:29 +0300

    Finish user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ActiveRecord::Base
 
  	has_secure_password
 	
-	validates :username , presence: true , length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validate :email , presence: true , format: { with: VALID_EMAIL_REGEX } , uniqueness: { case_sensitive: false }
-	validate :password ,presence: true , length: { minimum:  6 }
-	validate :password_confirmation , presence: true 
+  validates :username, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
