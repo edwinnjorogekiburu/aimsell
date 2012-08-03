6b6346e64145a83611bf523a2cb98739638b5e9d
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-08-03 12:04:03 +0300

    Gem file edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Aimsell::Application.routes.draw do
   resources :users
 
   root to: 'static_pages#home'
+  
   match '/home', to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
